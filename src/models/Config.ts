7223beabbec79df0cc1fa6a3800f445ca602fe59
Arnau Sanchez <pyarnau@gmail.com>
2020-03-06 13:24:52 +0100

    Add Config/User base models

---
+++ src/models/Config.ts
@@ -0,0 +1,44 @@
+import { D2Api, MetadataPick } from "d2-api";
+
+const baseConfig = {
+    // Add here static configuration
+};
+
+export type BaseConfig = typeof baseConfig;
+
+interface ConfigData {
+    base: BaseConfig;
+    categoryCombos: CategoryCombo[];
+}
+
+interface CategoryCombo {
+    id: string;
+    code: string;
+}
+
+const metadataParams = {
+    categoryCombos: {
+        fields: { id: true, code: true },
+    },
+} as const;
+
+export type Metadata = MetadataPick<typeof metadataParams>;
+
+export class Config {
+    constructor(private api: D2Api, private data: ConfigData) {}
+
+    public get<Key extends keyof ConfigData>(key: Key): ConfigData[Key] {
+        return this.data[key];
+    }
+
+    static async build(api: D2Api): Promise<Config> {
+        const metadata = await api.metadata.get(metadataParams).getData();
+
+        const data: ConfigData = {
+            base: baseConfig,
+            categoryCombos: metadata.categoryCombos,
+        };
+
+        return new Config(api, data);
+    }
+}
