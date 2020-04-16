3aed8bfdf21981fd5933c2fa5decc1a600570209
Adrian Quintana <adrianquintana@gmail.com>
2020-04-16 14:56:29 +0100

    use static lib for ee

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -21,7 +21,17 @@ import { AppContext } from "../../contexts/app-context";
 import { Config } from "../../models/Config";
 import { User } from "../../models/User";
 import { LinearProgress } from "@material-ui/core";
-import ee from "@google/earthengine";
+// import ee from "@google/earthengine";
+
+// declare const ee: any;
+
+declare global {
+    interface Window {
+        ee: any;
+    }
+}
+
+const ee = window.ee || {};
 
 type D2 = object;
 
