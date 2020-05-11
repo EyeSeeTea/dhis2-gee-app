891e95980f7dd19539c1452c208d5b6d87392526
Jorge Sánchez <xurxosf@gmail.com>
2020-05-11 11:38:16 +0200

    Fix bug saving existed mapping

--- src/webapp/models/Mapping.ts
+++ src/webapp/models/Mapping.ts
@@ -157,15 +157,14 @@ class Mapping {
         const dataStore = getDataStore(api, config);
         const mappingsKey = config.data.base.dataStore.keys.mappings;
         const mappingsById = await dataStore.get<Mapping[] | undefined>(mappingsKey).getData();
+
         const newMappingsList = {
             ...mappingsById,
             [this.id]: {
-                ...this.data,
-                attributeMappingDictionary: _(this.data.attributeMappingDictionary).mapValues(
-                    am => am.data
-                ),
+                ...this.data
             },
         };
+
         await dataStore.save(mappingsKey, newMappingsList).getData();
     }
 }
