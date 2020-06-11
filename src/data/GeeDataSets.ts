aaabaef2c2eaa4720637a9c3e9a887028cdd671d
Jorge Sánchez <xurxosf@gmail.com>
2020-06-11 18:28:39 +0200

    add some gee datasets

--- src/data/GeeDataSets.ts
+++ src/data/GeeDataSets.ts
@@ -60,6 +60,60 @@ export const geeDataSets: { [name: string]: any } = {
         ],
         doc: "https://developers.google.com/earth-engine/datasets/catalog/UCSB-CHG_CHIRPS_DAILY",
     },
+    era5Montly: {
+        displayName: "ERA5 - Montly",
+        description:
+            "Latest climate reanalysis produced by ECMWF / Copernicus Climate Change Service",
+        pointer: "ECMWF/ERA5/MONTHLY",
+        bands: [
+            {
+                name: "mean_2m_air_temperature",
+                units: "K",
+                description: "Average air temperature at 2m height (daily average)",
+            },
+            {
+                name: "minimum_2m_air_temperature",
+                units: "K",
+                description: "Minimum air temperature at 2m height (daily minimum)",
+            },
+            {
+                name: "maximum_2m_air_temperature",
+                units: "K",
+                description: "Maximum air temperature at 2m height (daily maximum)",
+            },
+            {
+                name: "dewpoint_2m_temperature",
+                units: "K",
+                description: "Dewpoint temperature at 2m height (daily average)",
+            },
+            {
+                name: "total_precipitation",
+                units: "m",
+                description: "Total precipitation (daily sums)",
+            },
+            {
+                name: "surface_pressure",
+                units: "Pa",
+                description: "Surface pressure (daily average)",
+            },
+            {
+                name: "mean_sea_level_pressure",
+                units: "Pa",
+                description: "Mean sea level pressure (daily average)",
+            },
+            {
+                name: "u_component_of_wind_10m",
+                units: "m s-1",
+                description: "10m u-component of wind (daily average)",
+            },
+            {
+                name: "v_component_of_wind_10m",
+                units: "m s-1",
+                description: "10m v-component of wind (daily average)",
+            },
+        ],
+        doc: "https://developers.google.com/earth-engine/datasets/catalog/UCSB-CHG_CHIRPS_DAILY",
+    },
     daymetV3: {
         displayName: "DAYMET V3",
         description: "Daily Surface Weather and Climatological Summaries",
@@ -98,5 +152,12 @@ export const geeDataSets: { [name: string]: any } = {
             },
         ],
         doc: "https://developers.google.com/earth-engine/datasets/catalog/NASA_ORNL_DAYMET_V3",
+    },
+    waterClassYearly: {
+        displayName: "Yearly Water Classification History",
+        description: "JRC Yearly Water Classification History, v1.1",
+        pointer: "JRC/GSW1_1/YearlyHistory",
+        bands: [{ name: "waterClass", units: "", description: "Classification of the seasonality of water throughout the year." }],
+        doc: "https://developers.google.com/earth-engine/datasets/catalog/JRC_GSW1_1_YearlyHistory",
     },
 };
