10801faee69a67a482bfaff3a4565d82ef2d87b5
fdelemarre <florian.delemarre@365talents.com>
2024-08-08 09:27:29 +0200

    getAllWithCoordinates fetch by recursive (batch size 1000)

--- src/data/OrgUnitD2ApiRepository.ts
+++ src/data/OrgUnitD2ApiRepository.ts
@@ -2,6 +2,7 @@ import { Id } from "@eyeseetea/d2-api";
 import { OrgUnit } from "../domain/entities/OrgUnit";
 import OrgUnitRepository from "../domain/repositories/OrgUnitRepository";
 import { D2Api } from "../types/d2-api";
+import _ from "lodash";
 
 class OrgUnitD2ApiRepository implements OrgUnitRepository {
     constructor(private d2Api: D2Api) {}
@@ -19,15 +20,44 @@ class OrgUnitD2ApiRepository implements OrgUnitRepository {
     }
 
     async getAllWithCoordinates(): Promise<OrgUnit[]> {
-        const response = await this.d2Api
-            .get<{ organisationUnits: OrgUnit[] }>("/organisationUnits", {
-                paging: false,
+        // First, fetch the first page to get the total number of pages
+        const initialResponse = await this.d2Api
+            .get<{ organisationUnits: OrgUnit[]; pager: { page: number; pageCount: number } }>("/organisationUnits", {
+                paging: true,
+                pageSize: 1000, // Fetch in batches of 1000
                 fields: "id,geometry",
                 filter: "geometry:!null",
+                page: 1,
             })
             .getData();
 
-        return response.organisationUnits;
+        const totalPages = initialResponse.pager.pageCount;
+
+        // Create an array of page numbers to fetch
+        const pageNumbers = _(totalPages)
+            .range(1) // Generate numbers from 2 to totalPages
+            .reverse()
+            .value();
+
+        // Map each page number to a fetch promise
+        const fetchPromises = pageNumbers.map(page =>
+            this.d2Api
+                .get<{ organisationUnits: OrgUnit[] }>("/organisationUnits", {
+                    paging: true,
+                    pageSize: 1000,
+                    fields: "id,geometry",
+                    filter: "geometry:!null",
+                    page: page,
+                })
+                .getData()
+                .then(response => response.organisationUnits)
+        );
+
+        // Use Promise.all to wait for all fetch promises to resolve
+        const pages = await Promise.all(fetchPromises);
+
+        // Flatten the array of pages into a single array of OrgUnits
+        return [initialResponse.organisationUnits, ...pages].flat();
     }
 }
 
